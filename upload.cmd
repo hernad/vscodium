88cbb0088f55f531fa07a6e504d49255cc74563b
Ernad Husremović <hernad@bring.out.ba>
2021-12-24 10:56:37 +0100

    product.json set dataFolderName

---
+++ upload.cmd
@@ -0,0 +1,7 @@
+set VER=1.63.2
+
+..\cygwin64\bin\scp vscode/.build/win32-x64/user-setup/VSCodeSetup.exe root@192.168.168.251:/var/www/html/eShell/eShellSetup-x64-%VER%.exe
+VSCodeSetup.exe                                                                                         100%   78MB   4.0MB/s   00:19
+
+..\cygwin64\bin\scp vscode/.build/win32-x64/user-setup/VSCodeSetup.exe root@192.168.168.252:/var/www/html/eShell/eShellSetup-x64-%VER%.exe
+VSCodeSetup.exe
