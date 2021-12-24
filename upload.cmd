87cbf599038bf30129e4fab964edc8664b74795c
Ernad Husremović <hernad@bring.out.ba>
2021-12-24 14:43:10 +0100

    upload to http://download.bring.out.ba

--- upload.cmd
+++ upload.cmd
@@ -5,3 +5,5 @@ set VER=1.63.2
 
 ..\cygwin64\bin\scp vscode/.build/win32-x64/user-setup/VSCodeSetup.exe root@192.168.168.252:/var/www/html/eShell/eShellSetup-x64-%VER%.exe
 
+ssh root@192.168.168.251 chmod +r /var/www/html/eShell/eShellSetup*.exe
+ssh root@192.168.168.252 chmod +r /var/www/html/eShell/eShellSetup*.exe
